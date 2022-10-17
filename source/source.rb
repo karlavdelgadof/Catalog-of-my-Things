2482895a4e5a6dac90fa1d2d718394347ecb57ff
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-17 15:49:42 -0500

    Create add_item and list_all methods (set up association with item)

--- source/source.rb
+++ source/source.rb
@@ -1,10 +1,23 @@
 require 'securerandom'
+
 class Source
     attr_accessor :id, :name
     attr_reader :items
+
     def initialize(name)
         @id = SecureRandom.random_number(1000)
         @name = name
         @items = []
-    end    
+    end
+    
+    def add_item(item)
+        @items << item unless @items.include?(item)
+        item.source = self
+    end
+
+    def self.list_all_sources(sources)
+        sources each do |source|
+            puts "- Name: #{source.name}"
+        end
+    end
 end

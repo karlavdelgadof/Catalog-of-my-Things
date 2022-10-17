216eca4493a5dea0848f02996e6ff8a6443ae633
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-17 13:28:14 -0500

    Create run method for app class

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # gem "rails"
 gem 'colorize', '~> 0.8.1'
 gem 'json', '~> 2.6', '>= 2.6.2'
-gem 'rspec', '~> 3.4' 
+gem 'rspec', '~> 3.4'
 gem 'rubocop', '>= 1.0', '< 2.0'
--- app.rb
+++ app.rb
@@ -1,47 +1,70 @@
 class App
-    def options
-        [
-            '[1] => List all books'
-            '[2] => List all music albums'
-            '[3] => List all movies'
-            '[4] => List all games'
-            '[5] => List all genres'
-            '[6] => List all labels'
-            '[7] => List all authors'
-            '[8] => List all sources'
-            '[9] => Add a book'
-            '[10] => Add a music album'
-            '[11] => Add a movie'
-            '[12] => Add a game'
-        ]
-    end
-    
-    def check_selection(response)
-        case response
-        when '1'
-            list_all_books
-        when '2'
-            list_all_music_albums
-        when '3'
-            lits_all_movies
-        when '4'
-            list_all_games
-        when '5'
-            list_all_genres
-        when '6'
-            list_all_labels
-        when '7'
-            list_all_authors
-        when '8'
-            list_all_sources
-        when '9'
-            create_book
-        when '10'
-            create_music_album
-        when '11'
-            create_movie
-        when '12'
-            create_game            
+  def options
+    [
+      '[1] => List all books',
+      '[2] => List all music albums',
+      '[3] => List all movies',
+      '[4] => List all games',
+      '[5] => List all genres',
+      '[6] => List all labels',
+      '[7] => List all authors',
+      '[8] => List all sources',
+      '[9] => Add a book',
+      '[10] => Add a music album',
+      '[11] => Add a movie',
+      '[12] => Add a game',
+      '[13] => Exit'
+    ]
+  end
+
+  def run
+    user_response = 0
+    puts "\n\nWelcome to the Catalog of my Things!\n\n".colorize(color: :green).bold
+
+    while user_response != '13'
+      puts "Please choose an option by entering a number:\n\n".colorize(color: :magenta).italic
+      options.each do |choice|
+        if choice.include?('Exit')
+          puts choice.colorize(color: :red)
+        else
+          puts choice
         end
-    end 
+      end
+      print "\n\nEnter Option [number]: ".colorize(color: :white).bold
+      user_response = gets.chomp
+      puts "\n\n"
+      check_selection(user_response)
+    end
+
+    puts "Thank you for using this app!\n\n".colorize(color: :cyan).bold if user_response == '13'
+  end
+
+  def check_selection(response)
+    case response
+    when '1'
+      list_all_books
+    when '2'
+      list_all_music_albums
+    when '3'
+      lits_all_movies
+    when '4'
+      list_all_games
+    when '5'
+      list_all_genres
+    when '6'
+      list_all_labels
+    when '7'
+      list_all_authors
+    when '8'
+      list_all_sources
+    when '9'
+      create_book
+    when '10'
+      create_music_album
+    when '11'
+      create_movie
+    when '12'
+      create_game
+    end
+  end
 end

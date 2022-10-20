59fed9a8234eb8936809bc3c9e24efc27c183b23
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-20 08:41:00 -0500

    Create classes to handle data storage

---
+++ data-storage/user_output.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class UserOutput
+  def self.load_albums(albums)
+    return albums unless File.exist?('./json/albums.json')
+
+    object = JSON.parse(File.read('./json/albums.json'))
+    object.each do |album|
+      album_json = MusicAlbum.new(album['release_date'], album['title'], album['genre'], album['on_spotify'])
+      albums << album_json
+    end
+  end
+
+  def self.load_genres(genres)
+    return genres unless File.exist?('./json/genres.json')
+
+    object = JSON.parse(File.read('./json/genres.json'))
+    object.each do |genre|
+      genre_json = Genre.new(genre['name'])
+      genre_json.id = genre['id']
+      genres << genre_json
+    end
+  end
+
+  def self.load_data(albums, genres)
+    load_albums(albums)
+    load_genres(genres)
+  end
+end
